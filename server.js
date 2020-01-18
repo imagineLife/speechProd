04fc45ce3ebb043b5ab497614f5634fac9ade60f
Eric Laursen <ericjlaursen@gmail.com>
2020-01-17 19:11:12 -0500

    server file

---
+++ server.js
@@ -0,0 +1,19 @@
+/*
+  To serve the app in production
+*/
+
+const express = require('express');
+const path = require('path');
+const app = express();
+const targetPort = process.env.PORT || 9006;
+let staticPath = path.join(__dirname, '/')
+const xprsStatic = express.static(staticPath);
+
+app.use(xprsStatic);
+
+// eslint-disable-next-line flowtype/require-parameter-type
+app.use(function(req, res) {
+  res.sendFile(path.join(__dirname, '/', 'index.html'));
+});
+app.listen(targetPort);
+console.log(`Application listening on ${targetPort}`);
