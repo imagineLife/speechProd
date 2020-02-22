eb173d12ba2c4ef315d63755ed1520677b160fb0
Eric Laursen <ericjlaursen@gmail.com>
2020-02-22 05:49:08 -0500

    new version

--- data/themeCreator.js
+++ data/themeCreator.js
@@ -1,4 +1,4 @@
-const fs = require('fs')
+const fs = require('fs');
 
 
 /*
@@ -12,34 +12,32 @@ const fs = require('fs')
 			themeWord3: [array,or,words,that,point,to,the,theme,word3]
 		}
 */
-fs.readFile('./themes.tsv', 'utf8', (err,data) => {
-	if(err){
-		console.log('ERROR')
-		console.log(err)
-		return;
-	}
-	let parsed = data.split('\n')
+fs.readFile('./themes.tsv', 'utf8', (err, data) => {
+  if (err) {
+    console.log('ERROR');
+    console.log(err);
+    return;
+  }
+  const parsed = data.split('\n');
 
-	let themeObj = {}
-	
-	//loop through tsv lines
-	parsed.forEach((d,dataIndex) => {
+  const themeObj = {};
 
-		//split line into theme & sentenceInddex
-		let splat = d.split('\t')
-		let thisTheme = splat[1]
-		let arrOfKeyword = splat[2].split(',')
-		
+  // loop through tsv lines
+  parsed.forEach((d, dataIndex) => {
+    // split line into theme & sentenceInddex
+    const splat = d.split('\t');
+    const thisTheme = splat[1];
+    const arrOfKeyword = splat[2].split(',');
 
-		if(themeObj[thisTheme]){
-			let newArr = [...themeObj[thisTheme],...arrOfKeyword]
-			themeObj[thisTheme] = [...new Set([...newArr])]
-		}else{
-			themeObj[thisTheme] = arrOfKeyword
-		}
-	})
 
-	console.log('themeObj')
-	console.dir(themeObj)
-	
-})
+    if (themeObj[thisTheme]) {
+      const newArr = [...themeObj[thisTheme], ...arrOfKeyword];
+      themeObj[thisTheme] = [...new Set([...newArr])];
+    } else {
+      themeObj[thisTheme] = arrOfKeyword;
+    }
+  });
+
+  console.log('themeObj');
+  console.dir(themeObj);
+});
